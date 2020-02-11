af3c0e4ae66e153950c0bfef756004a06c5b79f8
rupeshravindran <rupeshravindran@yahoo.co.uk>
2020-02-11 12:04:20 +0000

    Update Grid.test.js

--- src/component/Grid.test.js
+++ src/component/Grid.test.js
@@ -4,16 +4,25 @@ import {columns} from '../config'
 import {Grid} from './Grid';
 
 test('renders loan calculator', () => {
-  const data = [{date:'12/02/2020', principal: 10000, interest: 300, total: 10000}];
+  const data = [{date:'12/02/2020', principal: 10000, interest: 300, total: 10300}];
   const { getByText } = render(<Grid id={'#11'} data={data} columns={columns}/>);
   const date = getByText('12/02/2020');
-  //const principal = getByText('£10,000');
-  //const interest = getByText('300');
-  //const total = getByText('£10,000');
+  const principal = getByText('£10,300.00');
+  const interest = getByText('£300.00');
+  const total = getByText('£10,300.00');
+
+  const dateHeader = getByText(/date/i);
+  const principalHeader = getByText(/principal/i);
+  const interestHeader = getByText(/interest/i);
+  const totalHeader = getByText(/total/i);
+
+  expect(dateHeader).toBeInTheDocument();
+  expect(principalHeader).toBeInTheDocument();
+  expect(interestHeader).toBeInTheDocument();
+  expect(totalHeader).toBeInTheDocument();
 
   expect(date).toBeInTheDocument();
-//   expect(principal).toBeInTheDocument();
-  
-//   expect(interest).toBeInTheDocument();
-//   expect(total).toBeInTheDocument();
+  expect(principal).toBeInTheDocument();
+  expect(interest).toBeInTheDocument();
+  expect(total).toBeInTheDocument();
 });
