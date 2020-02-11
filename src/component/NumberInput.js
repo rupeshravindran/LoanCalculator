a0d7593b0c34f85d28fbba6c15ebced4f746e0af
rupeshravindran <rupeshravindran@yahoo.co.uk>
2020-02-11 12:11:34 +0000

    Update NumberInput.js

--- src/component/NumberInput.js
+++ src/component/NumberInput.js
@@ -5,7 +5,7 @@ export function NumberInput({label, value, description, onChange}) {
   return (
     <div className="form-row">
         <label >{label}</label>
-        <input type="number" value={value} min='0' onChange={onChange}/>
+        <input data-testid="numberInput" type="number" value={value} min='0' onChange={onChange}/>
         <label>{description}</label>
     </div>
   );
